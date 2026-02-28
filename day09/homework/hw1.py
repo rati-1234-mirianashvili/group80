a338adce62bb020e12919a89965f4eda761edb11
rati-1234 <mirianashvili@gmail.com>
2026-02-28 12:18:14 +0400

    "added homework"

---
+++ day09/homework/hw1.py
@@ -0,0 +1,4 @@
+#)კომენტარებით ახსენით რას აკეთებს str(),int() და float() ფუნქციები.
+#str mnishvnelobas aqcevs teqstad
+#int mnishvnelobas aqcevs mtel ricxvad
+#float mnishvnelobas aqcevs aramtel ricxvad
